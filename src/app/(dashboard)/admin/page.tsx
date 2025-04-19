e479f44e44166ac4dd657035a77fe64d3be95d60
Manish <148551097+I-amManish@users.noreply.github.com>
2025-04-19 10:59:29 +0530

    feat: update admin page with attendance chart and layout adjustments; remove unused components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,13 @@
+"use client"; // Add this if you're not using dynamic imports
+
+import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
 import UserCards from "@/components/UserCards";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* LEFT */}
+      {/* LEFT SIDE */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
@@ -16,21 +19,23 @@ const AdminPage = () => {
 
         {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-          <CountChart />
+            <CountChart />
           </div>
-          {/* ATTENDENCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
           </div>
         </div>
 
-        {/* bottom CHARTS */}
-        <div className=""></div>
-
+        {/* BOTTOM PLACEHOLDER */}
+        {/* <div className="h-[200px] bg-gray-100 rounded-lg">More charts coming soon...</div> */}
       </div>
-      {/* RIGHT */}
-      <div className="w-full lg:w-1/3">r</div>
+
+      {/* RIGHT SIDE */}
+      <div className="w-full lg:w-1/3">
+        {/* Add your sidebar or extra widgets here */}
+        r
+      </div>
     </div>
   );
 };
