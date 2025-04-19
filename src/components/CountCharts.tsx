ad584d6fa0999c9abd5bc4cca2e830cc32aa9ae8
Manish <148551097+I-amManish@users.noreply.github.com>
2025-04-19 21:29:01 +0530

    refactor: clean up CountCharts component for improved readability

--- src/components/CountCharts.tsx
+++ src/components/CountCharts.tsx
@@ -61,10 +61,8 @@ export default class Example extends PureComponent {
       <ResponsiveContainer width="100%" height="100%">
         <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
           <RadialBar
-            minAngle={15}
             label={{ position: 'insideStart', fill: '#fff' }}
             background
-            clockWise
             dataKey="uv"
           />
           <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
