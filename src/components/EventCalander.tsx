f13c5d5813337bb835d225ceafdd161c8e71ccf5
Manish <148551097+I-amManish@users.noreply.github.com>
2025-04-20 21:35:50 +0530

    feat: add event calendar component with event display and styling

--- src/components/EventCalander.tsx
+++ src/components/EventCalander.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Image from "next/image";
+import { env } from "process";
 import { useState } from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+
+// TEMP DATA
 const events = [
     {
         id: 1,
@@ -34,6 +38,22 @@ const EventCalander = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <Calendar onChange={onChange} value={value} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" width={20}   height={20} alt="" className="" />
+      </div>
+
+      <div className="flex flex-col gap-4">
+        {events.map(event => (
+            <div className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-mySky even:border-t-myPurple" key={event.id}>
+                <div className="flex items-center justify-between ">
+                    <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                    <span className="text-gray-300 text-sm">{event.time}</span>
+                </div>
+                <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+            </div>
+        ))}
+      </div>
     </div>
   );
 };
